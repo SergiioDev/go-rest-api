2715139d4e47bb3193235e631c4ada4c9f888188
SergiioDev <sergi13456@hotmail.es>
2023-12-06 12:07:30 +0100

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SergiioDev/learning-go
+
+go 1.21.4
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Driver struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	LastName  string `json:"lastname"`
+	Team      string `json:"team"`
+	DateBirth string `json:"dateBirth"`
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/drivers", Drivers)
+	router.POST("/drivers", DriversCreate)
+	router.GET("/drivers/:name", DriverByName)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+var drivers = []Driver{
+	{ID: "1", Name: "Fernando", LastName: "Alonso", Team: "Aston Martin", DateBirth: "07/29/1981"},
+}
+
+func Drivers(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	log.Println("Handling GET /drivers request")
+	w.Header().Set("Content-Type", "application/json")
+	res, err := json.Marshal(drivers)
+	if err != nil {
+		fmt.Println("Error parsing json")
+	}
+	w.Write(res)
+}
+
+func DriversCreate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var driver Driver
+	w.Header().Set("Content-Type", "application/json")
+	json.NewDecoder(r.Body).Decode(&driver)
+	drivers = append(drivers, driver)
+	w.WriteHeader(http.StatusCreated)
+	res, err := json.Marshal(driver)
+	if err != nil {
+		fmt.Println("Error parsing driver into a json")
+	}
+	w.Write(res)
+}
+
+func DriverByName(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	driverName := params.ByName("name")
+	diver, err := findDriverByName(driverName)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+	res, err := json.Marshal(diver)
+	if err != nil {
+		http.Error(w, "Can Not parse driver into json", http.StatusNotFound)
+	}
+	w.Write(res)
+}
+
+func findDriverByName(name string) (Driver, error) {
+	for i := 0; i < len(drivers); i++ {
+		if drivers[i].Name == name {
+
+			fmt.Println(drivers[i].Name)
+			return drivers[i], nil
+		}
+	}
+	return Driver{}, errors.New("Can Not find driver with name " + name)
+}
